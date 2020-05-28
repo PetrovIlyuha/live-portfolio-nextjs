fb96d59e13468dfdad49774c7c3f690ae0d8a113
Ilya Petrov <dnamix1@gmail.com>
2020-05-28 14:17:36 +0300

    footer and navbar components implemented with Link form next lib

--- components/shared/Navbar.js
+++ components/shared/Navbar.js
@@ -1,66 +1,60 @@
 import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import Link from "next/link";
 
-const Navbar = () => {
+const AppLink = ({ children, className, href }) => (
+  <Link href={href}>
+    <a className={className}>{children}</a>
+  </Link>
+);
+
+const AppNavbar = () => {
   return (
     <div className="navbar-wrapper">
-      <nav className="navbar navbar-expand-lg navbar-dark fj-mw9">
-        <a className="navbar-brand mr-3 font-weight-bold" href="#">
-          Petrov Ilya
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav">
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">
-                Portfolio
-              </a>
-            </li>
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">
-                Courses
-              </a>
-            </li>
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">
-                CV
-              </a>
-            </li>
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">
-                Ask me
-              </a>
-            </li>
-          </ul>
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">
-                Sign Up
-              </a>
-            </li>
-            <li className="nav-item mr-3">
-              <a
-                className="nav-link btn btn-success bg-green-2 bright"
-                href="#"
-              >
-                Sign In
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar expand="lg" className="navbar-dark fj-mw9">
+        <Navbar.Brand className="mr-3 font-weight-bold">
+          <AppLink href="/" className="h4 text-white navbar-brand">
+            <img
+              src="/logo.png"
+              alt="Personal Brand Logo"
+              style={{
+                width: "120px",
+                borderRadius: "10px",
+                marginRight: "10px",
+                marginTop: 10,
+                boxShadow: "1px 0px 24px 10px rgba(119, 160, 118, 0.15)",
+              }}
+            />
+          </AppLink>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="mr-auto">
+            <AppLink href="/portfolios" className="text-center nav-link">
+              Portfolios
+            </AppLink>
+            <AppLink href="/forum/categories" className="text-center nav-link">
+              Forum
+            </AppLink>
+            <AppLink href="#" className="text-center nav-link">
+              CV
+            </AppLink>
+          </Nav>
+          <Nav>
+            <AppLink href="/login" className="text-center nav-link">
+              Login
+            </AppLink>
+            <AppLink
+              href="/register"
+              className="text-center btn btn-success bg-dark mx-auto pl-3 pr-3"
+            >
+              Register
+            </AppLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 };
 
-export default Navbar;
+export default AppNavbar;
