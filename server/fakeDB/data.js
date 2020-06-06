79b153594775cde068bd123a0cfab8665ceb9e6d
Ilya Petrov <dnamix1@gmail.com>
2020-06-06 13:52:43 +0300

    seed mock data and fake DB injectable class

---
+++ server/fakeDB/data.js
@@ -0,0 +1,30 @@
+const data = {
+  projects: [
+    {
+      title: "Instagram Clone with React",
+      content:
+        "Material UI and React with GraphQL based API clone of an Instagram App",
+      stack: "GraphQL, ReactJS, MaterialUI",
+      link: "https://instaclone-theta.now.sh/",
+      demoGif: "/insta_demo.gif",
+      daysInMaking: 34,
+      startDate: "2020-02-10T23:59Z",
+      endDate: "2020-02-29T23:59Z",
+      isInProgress: true,
+    },
+    {
+      title: "TeaHouse E-commerce SPA",
+      content:
+        "ReactJS front-end, Contentful as a headless CMS E-commerce App with Shopping Cart, Checkout pipeline sustained with data management in Context from React",
+      stack: "ReactJS, Contentful, React Context API as State manager",
+      link: "https://zenteahouse.netlify.app/",
+      demoGif: "/teahouse_demo.gif",
+      daysInMaking: 24,
+      startDate: "2020-04-03T23:59Z",
+      endDate: "2020-06-05T23:59Z",
+      isInProgress: false,
+    },
+  ],
+};
+
+module.exports = data;
