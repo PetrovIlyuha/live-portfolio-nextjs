0ff509b7b0fc8e2bbf9d3535289d3fe993cbf18a
Ilya Petrov <dnamix1@gmail.com>
2020-06-06 13:51:21 +0300

    lean apollo-express server

--- server/index.js
+++ server/index.js
@@ -1,45 +1,17 @@
 const express = require("express");
 const next = require("next");
 global.fetch = require("node-fetch");
-const { ApolloServer, gql } = require("apollo-server-express");
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-// resolvers
-const { projectQueries, projectMutations } = require("./graphql/resolvers");
-// types
-const { projectTypes } = require("./graphql/types");
+// mongo db connection
+require("./database").connect();
+
 app.prepare().then(() => {
   const server = express();
-
-  // graphql schema
-  const typeDefs = gql`
-    ${projectTypes}
-
-    type Query {
-      hello: String
-      project(id: ID): Project
-      projects: [Project]
-    }
-
-    type Mutation {
-      createProject(input: ProjectInput): Project
-      updateProject(id: ID, input: ProjectInput): Project
-      deleteProject(id: ID): ID
-    }
-  `;
-
-  // resolvers
-  const resolvers = {
-    Query: { ...projectQueries },
-    Mutation: { ...projectMutations },
-  };
-  const apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
+  const apolloServer = require("./graphql/index").createApolloServer();
   apolloServer.applyMiddleware({ app: server });
 
   server.all("*", (req, res) => {
