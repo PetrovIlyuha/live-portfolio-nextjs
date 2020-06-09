a5adf8a193368618a5722d4ff071d9e51a6c1c14
Ilya Petrov <dnamix1@gmail.com>
2020-06-09 14:21:37 +0300

    defined mutations to auth flow

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 const { ApolloServer, gql } = require("apollo-server-express");
 
-const { projectQueries, projectMutations } = require("./resolvers");
+const {
+  projectQueries,
+  projectMutations,
+  userMutations,
+} = require("./resolvers");
 const { projectTypes } = require("./types");
 const Project = require("./models/Project");
+const User = require("./models/User");
 
 exports.createApolloServer = () => {
   const typeDefs = gql`
@@ -18,13 +23,17 @@ exports.createApolloServer = () => {
       createProject(input: ProjectInput): Project
       updateProject(id: ID, input: ProjectInput): Project
       deleteProject(id: ID): ID
+
+      signIn: String
+      signUp: String
+      signOut: String
     }
   `;
 
   // resolvers
   const resolvers = {
     Query: { ...projectQueries },
-    Mutation: { ...projectMutations },
+    Mutation: { ...projectMutations, ...userMutations },
   };
   const apolloServer = new ApolloServer({
     typeDefs,
@@ -32,6 +41,7 @@ exports.createApolloServer = () => {
     context: () => ({
       models: {
         Project: new Project(mongoose.model("Project")),
+        User: new User(),
       },
     }),
   });
