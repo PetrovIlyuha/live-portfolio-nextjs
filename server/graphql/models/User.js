c334b48638f2b25d48fe99203a4e333ca4191ed4
Ilya Petrov <dnamix1@gmail.com>
2020-06-09 14:22:32 +0300

    user methods in class

---
+++ server/graphql/models/User.js
@@ -0,0 +1,18 @@
+class User {
+  // constructor(model) {
+  //   this.Model = model;
+  // }
+  signIn() {
+    return "Signing in";
+  }
+
+  signUp() {
+    return "Signing Up";
+  }
+
+  signOut() {
+    return "Signing Out";
+  }
+}
+
+module.exports = User;
