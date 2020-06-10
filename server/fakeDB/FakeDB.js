e27260486ca3e028fb1fe4d4de87aa0d3c6fc503
Ilya Petrov <dnamix1@gmail.com>
2020-06-10 21:20:57 +0300

    session configuration middleware and seed (for development only) data for the users's authentication with passport strategy

--- server/fakeDB/FakeDB.js
+++ server/fakeDB/FakeDB.js
@@ -1,13 +1,15 @@
-const { projects } = require("./data");
+const { projects, users } = require("./data");
 
 const Project = require("../database/models/project");
-
+const User = require("../database/models/user");
 class FakeDB {
   async cleanData() {
     await Project.deleteMany({});
+    await User.deleteMany({});
   }
 
   async addData() {
+    await User.create(users);
     await Project.create(projects);
   }
 
--- server/fakeDB/data.js
+++ server/fakeDB/data.js
@@ -1,4 +1,44 @@
+const mongoose = require("mongoose");
+
+const user1Id = mongoose.Types.ObjectId();
+const user2Id = mongoose.Types.ObjectId();
+const user3Id = mongoose.Types.ObjectId();
 const data = {
+  users: [
+    {
+      _id: user1Id,
+      avatar:
+        "https://images.generated.photos/dImmzScqAalv7N_B66mbEy6_KndIhKMwIpUWYP0CKFc/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA4NDYwOTAuanBn.jpg",
+      email: "dnamix1@gmail.com",
+      name: "Ilya Petrov",
+      username: "poweruser",
+      info: "Web-Developer from Moscow",
+      password: "superuser",
+      role: "admin",
+    },
+    {
+      _id: user2Id,
+      avatar:
+        "https://images.generated.photos/YSEY4NauMv8uQbbER1zXKftrxm_2NwhWkNrrkg9AMCU/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAxMjk2NjcuanBn.jpg",
+      email: "horge@gmail.com",
+      name: "Horhe Gomez",
+      username: "magicsteps",
+      info: "Web-Designer from Columbia",
+      password: "gomez123",
+      role: "guest",
+    },
+    {
+      _id: user3Id,
+      avatar:
+        "https://images.generated.photos/gRfW1sGEJTFDMU_Cn7ytjjyTIZX-peCmBuTsf2EZzw4/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAwMTc3MTIuanBn.jpg",
+      email: "veroclue@gmail.com",
+      name: "Veronica Cluevert",
+      username: "VeroClue",
+      info: "CTO of SaaS startup",
+      password: "veronica123",
+      role: "guest",
+    },
+  ],
   projects: [
     {
       title: "Instagram Clone with React",
@@ -11,6 +51,7 @@ const data = {
       startDate: "2020-02-10T23:59Z",
       endDate: "2020-02-29T23:59Z",
       isInProgress: true,
+      user: user1Id,
     },
     {
       title: "TeaHouse E-commerce SPA",
@@ -23,6 +64,7 @@ const data = {
       startDate: "2020-04-03T23:59Z",
       endDate: "2020-06-05T23:59Z",
       isInProgress: false,
+      user: user1Id,
     },
   ],
 };
