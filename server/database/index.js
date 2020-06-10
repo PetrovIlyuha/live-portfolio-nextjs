5fe7d6569e263453991b5f5c7868e88977ecffd0
Ilya Petrov <dnamix1@gmail.com>
2020-06-10 21:17:59 +0300

    initializing session storage within mongodb instance

--- server/database/index.js
+++ server/database/index.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 const config = require("../config/dev");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 require("./models/project");
+require("./models/user");
 
 exports.connect = () => {
   mongoose.connect(
@@ -10,9 +13,18 @@ exports.connect = () => {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useFindAndModify: false,
+      useCreateIndex: true,
     },
     () => {
       console.log("Mongo Atlas Cluster successfully connected ✅");
     }
   );
+};
+
+exports.initSessionStore = () => {
+  const store = new MongoDBStore({
+    uri: config.DB_URI,
+    collection: "projectSessions",
+  });
+  return store;
 };
