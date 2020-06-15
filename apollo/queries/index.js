0fec073b65871c41e6467e90365abdd9062fdddb
Ilya Petrov <dnamix1@gmail.com>
2020-06-15 16:04:47 +0300

    auth queries definitions themselves

--- apollo/queries/index.js
+++ apollo/queries/index.js
@@ -96,3 +96,52 @@ export const DELETE_PROJECT = gql`
     deleteProject(id: $id)
   }
 `;
+
+// Auth queries
+export const SIGN_UP = gql`
+  mutation SignUp(
+    $avatar: String
+    $username: String!
+    $email: String!
+    $password: String!
+    $passwordConfirmation: String!
+  ) {
+    signUp(
+      input: {
+        avatar: $avatar
+        username: $username
+        email: $email
+        password: $password
+        passwordConfirmation: $passwordConfirmation
+      }
+    )
+  }
+`;
+
+export const SIGN_IN = gql`
+  mutation SignIn($email: String!, $password: String!) {
+    signIn(input: { email: $email, password: $password }) {
+      _id
+      username
+      role
+      avatar
+    }
+  }
+`;
+
+export const SIGN_OUT = gql`
+  mutation SignOut {
+    signOut
+  }
+`;
+
+export const GET_USER = gql`
+  query User {
+    user {
+      _id
+      username
+      avatar
+      role
+    }
+  }
+`;
