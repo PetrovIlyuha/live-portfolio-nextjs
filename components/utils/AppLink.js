9ada8829e96a3800056df9381dd5da3eee0b3e4f
PetrovIlyuha <dnamix1@gmail.com>
2020-06-30 16:08:13 +0300

    major cleanup commit

--- components/utils/AppLink.js
+++ components/utils/AppLink.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Link from "next/link";
 
-const AppLink = ({ children, className, href }) => (
-  <Link href={href}>
+const AppLink = ({ children, className, href, as }) => (
+  <Link href={href} as={as}>
     <a className={className}>{children}</a>
   </Link>
 );
