472b834f38a5bad0abdb091572c6d9b7dc8eb191
PetrovIlyuha <dnamix1@gmail.com>
2020-06-30 16:10:40 +0300

    Project model's findAndUpdate method acception of validators on the fields, dashboard panel render projects

--- pages/instructor/[id]/dashboard.js
+++ pages/instructor/[id]/dashboard.js
@@ -1,37 +1,58 @@
-import withApollo from "@/hoc/withApollo";
-import withAuth from "@/hoc/withAuth";
-import { useRouter } from "next/router";
-import BaseLayout from "../../../layouts/BaseLayout";
-import { Card, Button } from "react-bootstrap";
+import withApollo from '@/hoc/withApollo';
+import withAuth from '@/hoc/withAuth';
+import { useRouter } from 'next/router';
+import BaseLayout from '../../../layouts/BaseLayout';
+import { Card, Button } from 'react-bootstrap';
+import { getDataFromTree } from '@apollo/react-ssr';
+import { useGetUserProjects, useDeleteProject } from '@/apollo/actions';
+import Link from 'next/link';
 
-const InstructorDashboard = () => {
+const InstructorDashboard = withAuth(() => {
   const router = useRouter();
-  const instructorId = router.query.id || "";
+  const { data } = useGetUserProjects();
+  const [deleteProject] = useDeleteProject();
+  const userProjects = (data && data.userProjects) || [];
+  // console.log(userProjects);
   return (
     <BaseLayout>
       <div className="container h-100">
         <div className="bwm-form mt-5">
           <div className="row">
             <div className="col-md-12">
-              <h1 className="page-title">
-                Instructor Projects - {instructorId}
-              </h1>
-              <Card>
-                <Card.Header>Featured</Card.Header>
-                <Card.Body>
-                  <Card.Title>Project Title</Card.Title>
-                  <Card.Text>test test test test...</Card.Text>
-                  <Button variant="outline-primary">Link to somewhere</Button>
-                </Card.Body>
-              </Card>
+              <h1 className="page-title">Instructor's Projects</h1>
+              {userProjects.map((project, index) => (
+                <Card className="mt-5" key={index}>
+                  <Card.Header>{project.title}</Card.Header>
+                  <Card.Body>
+                    <Card.Title>{project.title}</Card.Title>
+                    <Card.Text>{project.stack}</Card.Text>
+                    <Card.Text>
+                      {project.startDate} -{' '}
+                      {project.endDate || 'currently in progress'}
+                    </Card.Text>
+                    <Link
+                      href="/projects/[id]/edit"
+                      as={`/projects/${project._id}/edit`}
+                    >
+                      <a className="btn btn-warning mr-3">Update</a>
+                    </Link>
+                    <Button
+                      variant="danger"
+                      onClick={() =>
+                        deleteProject({ variables: { id: project._id } })
+                      }
+                    >
+                      Delete
+                    </Button>
+                  </Card.Body>
+                </Card>
+              ))}
             </div>
           </div>
         </div>
       </div>
     </BaseLayout>
   );
-};
+}, ['admin', 'instructor']);
 
-export default withApollo(
-  withAuth(InstructorDashboard, ["admin", "instructor"])
-);
+export default withApollo(InstructorDashboard, { getDataFromTree });
