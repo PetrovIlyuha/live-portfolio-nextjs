c5734e68c0b3ced492cec6b27d18a2a2aa7ea549
PetrovIlyuha <dnamix1@gmail.com>
2020-07-01 14:16:47 +0300

    toastify setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import "@/styles/index.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "react-datepicker/dist/react-datepicker.css";
+import '@/styles/index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
 
