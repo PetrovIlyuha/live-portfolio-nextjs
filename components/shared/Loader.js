cc3f534b614597f9f3c606b53f0576c0b615e722
PetrovIlyuha <dnamix1@gmail.com>
2020-07-02 15:36:13 +0300

    spinner loader template and scss styling

---
+++ components/shared/Loader.js
@@ -0,0 +1,12 @@
+const SpinnerLoader = ({ variant = 'normal' }) => (
+  <div className={`sk-chase sk-chase-${variant}`}>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+  </div>
+);
+
+export default SpinnerLoader;
