a9745d108a73f7c5a4ba9c2744725ebff4b8326d
PetrovIlyuha <dnamix1@gmail.com>
2020-07-02 15:38:46 +0300

    messages hash-table and 404 image

---
+++ variables/messages.js
@@ -0,0 +1,7 @@
+export const messages = {
+  NOT_AUTHENTICATED: 'You should be logged in to visit this page!',
+  NOT_AUTHORIZED: 'You lack access rights to visit this page!',
+  LOGGED_IN: 'We Wish you a productive time! 🔥',
+  REGISTERED: 'Registration success. Proceed to log in!',
+  LOGGED_OUT: 'Oh.., OK! See you soon!',
+};
