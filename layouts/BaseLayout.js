3a678727f4c8c254f510554daccef2f7f52b9acb
PetrovIlyuha <dnamix1@gmail.com>
2020-07-03 17:06:04 +0300

    messaging functionality within Base Layout component

--- layouts/BaseLayout.js
+++ layouts/BaseLayout.js
@@ -2,14 +2,42 @@ import Navbar from '@/components/shared/Navbar';
 import Hero from '@/components/shared/Hero';
 import Footer from '@/components/shared/Footer';
 import { ToastContainer } from 'react-toastify';
+import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
+import { messages } from '../variables/messages';
 
 const BaseLayout = ({ children, page = '', footer = 'absolute' }) => {
+  const router = useRouter();
+  const { message } = router.query;
+
+  const displayMessage = () => {
+    if (message === 'LOGGED_IN') {
+      toast.success(messages[message], {
+        hideProgressBar: true,
+        autoClose: 5000,
+        pauseOnHover: true,
+      });
+    } else if (message === 'LOGGED_OUT') {
+      toast.success(messages[message], {
+        hideProgressBar: true,
+        autoClose: 5000,
+        pauseOnHover: true,
+      });
+    } else {
+      toast.error(messages[message], {
+        hideProgressBar: true,
+        autoClose: 5000,
+        pauseOnHover: true,
+      });
+    }
+  };
   const isHomePage = () => page === 'Home';
   return (
     <div className="portfolio-app">
       <Navbar />
+      {message && displayMessage()}
       {isHomePage() && <Hero />}
-      <div>{children}</div>
+      <div style={{ marginTop: '80px' }}>{children}</div>
       <div className="text-center h4">
         <Footer footer={footer} />
       </div>
