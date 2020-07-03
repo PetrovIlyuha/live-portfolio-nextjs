b8a86a919aeacf4b2c73dd6342028f29f0d15179
PetrovIlyuha <dnamix1@gmail.com>
2020-07-03 17:08:57 +0300

    login page proper footer positioning and messaging implemented

--- pages/login.js
+++ pages/login.js
@@ -1,20 +1,40 @@
-import LoginForm from "../components/forms/LoginForm";
-import withApollo from "@/hoc/withApollo";
-import { useSignIn } from "@/apollo/actions";
-import Redirect from "../components/shared/Redirect";
-import BaseLayout from "../layouts/BaseLayout";
+import LoginForm from '../components/forms/LoginForm';
+import withApollo from '@/hoc/withApollo';
+import { useSignIn } from '@/apollo/actions';
+import { useRouter } from 'next/router';
+import Redirect from '../components/shared/Redirect';
+import BaseLayout from '../layouts/BaseLayout';
+import { toast } from 'react-toastify';
+import { messages } from '../variables/messages';
 
 const Login = () => {
   const [signIn, { data, loading, error }] = useSignIn();
-
-  const errorMessage = (error) => {
+  const router = useRouter();
+  const { message } = router.query;
+  const errorMessage = error => {
     return (
       (error.graphQLErrors && error.graphQLErrors[0].message) ||
-      "Something went wrong!"
+      'Something went wrong!'
     );
   };
+
+  const accessViolation = () => {
+    if (message === 'REGISTERED') {
+      toast.success(messages[message], {
+        hideProgressBar: true,
+        autoClose: 4000,
+        pauseOnHover: true,
+      });
+    } else {
+      toast.error(messages[message], {
+        hideProgressBar: true,
+        autoClose: 5000,
+        pauseOnHover: true,
+      });
+    }
+  };
   return (
-    <BaseLayout>
+    <BaseLayout footer="absolute">
       <div className="container">
         <section className="section-title">
           <div className="px-2">
@@ -27,11 +47,14 @@ const Login = () => {
           <div className="row">
             <div className="col-md-5 mx-auto">
               <h1 className="page-title">Login</h1>
+              {message && accessViolation()}
               <LoginForm
                 loading={loading}
-                onSubmit={(signInData) => signIn({ variables: signInData })}
+                onSubmit={signInData => signIn({ variables: signInData })}
               />
-              {data && data.signIn && <Redirect toPage="/" />}
+              {data && data.signIn && (
+                <Redirect toPage="/" query={{ message: 'LOGGED_IN' }} />
+              )}
               {error && (
                 <div className="alert alert-danger">{errorMessage(error)}</div>
               )}
