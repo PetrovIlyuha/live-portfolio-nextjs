6a2f536e340ff389c36aace2781b5b9d1acf73f3
PetrovIlyuha <dnamix1@gmail.com>
2020-07-10 16:08:43 +0300

    population of the post parent with the user, date formatter, and massive re-styling

--- utils/functions.js
+++ utils/functions.js
@@ -3,3 +3,5 @@ import moment from 'moment';
 export const formatDate = date => {
   return moment.unix(date / 1000).format('MMMM Do YYYY');
 };
+
+export const fromNow = date => moment.unix(date / 1000).fromNow();
