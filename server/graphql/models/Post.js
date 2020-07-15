cb37847157a61a6ceccfcf36f181449628a379cc
PetrovIlyuha <dnamix1@gmail.com>
2020-07-15 12:28:26 +0300

    create post graphql model method implementation

--- server/graphql/models/Post.js
+++ server/graphql/models/Post.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 class Post {
   constructor(model, user) {
     this.Model = model;
@@ -8,6 +10,31 @@ class Post {
       .sort('createdAt')
       .populate('topic')
       .populate('user')
+      .populate({ path: 'parent', populate: 'user' });
+  }
+  async create(post) {
+    console.log(this.user);
+    if (!this.user) {
+      throw new Error("You're not authenticated to create Posts!");
+    }
+    post.user = this.user;
+    const createdAt = moment().toISOString();
+    const slugPart = `-${(Math.random().toString(16) + '0000000').substr(
+      2,
+      8
+    )}`;
+    const fullSlugPart = createdAt + ':' + slugPart;
+    if (post.parent) {
+      const parent = await this.Model.findById(post.parent);
+      post.slug = parent.slug + '/' + slugPart;
+      post.fullSlug = parent.fullSlug + '/' + fullSlugPart;
+    } else {
+      post.slug = slugPart;
+      post.fullSlug = fullSlugPart;
+    }
+    const createdPost = await this.Model.create(post);
+    return this.Model.findById(createdPost._id)
+      .populate('topic')
       .populate({ path: 'parent', populate: 'user' });
   }
 }
