6b651b366ecbcc532b0a9c8d543f4519e4e47c9a
PetrovIlyuha <dnamix1@gmail.com>
2020-07-20 11:24:38 +0300

    long-polling of posts data evert 10 seconds and adding pagination to posts pages

--- pages/forum/topics/[slug].js
+++ pages/forum/topics/[slug].js
@@ -12,26 +12,32 @@ import { getDataFromTree } from '@apollo/react-ssr';
 import PostItem from '../../../components/forum/PostItem';
 import Replier from '../../../components/shared/Replier';
 import { toast } from 'react-toastify';
-import AppPagination from '@/components/shared/Pagination';
+import AppPagination from '../../../components/shared/Pagination';
 
-const useInitialData = () => {
-  const router = useRouter();
-  const { slug } = router.query;
+const useInitialData = (slug, pagination) => {
   const { data } = useGetTopicBySlug({ variables: { slug } });
   const { data: userData } = useGetUser();
   const { data: postsByTopicData, fetchMore } = useGetPostsByTopic({
-    variables: { slug },
+    variables: { slug, ...pagination },
+    pollInterval: 10000,
   });
   const topic = (data && data.topicBySlug) || {};
   const postsData = (postsByTopicData && postsByTopicData.postsByTopic) || {
     posts: [],
+    count: 0,
   };
   const user = (userData && userData.user) || null;
   return { topic, ...postsData, user, fetchMore };
 };
 
 const PostPage = () => {
-  const { topic, posts, ...rest } = useInitialData();
+  const router = useRouter();
+  const { slug, pageNum = 1, pageSize = 5 } = router.query;
+  const [pagination, setPagination] = useState({
+    pageNum: parseInt(pageNum, 10),
+    pageSize: parseInt(pageSize, 10),
+  });
+  const { topic, posts, ...rest } = useInitialData(slug, pagination);
   return (
     <BaseLayout>
       <div className="container">
@@ -42,17 +48,38 @@ const PostPage = () => {
             </div>
           </div>
         </section>
-        <Posts topic={topic} posts={posts} {...rest} />
+        <Posts
+          {...pagination}
+          topic={topic}
+          posts={posts}
+          {...rest}
+          onPageChange={(pageNum, pageSize) => {
+            router.replace(
+              '/forum/topics/[slug]',
+              `/forum/topics/${slug}?pageNum=${pageNum}&pageSize=${pageSize}`,
+              { shallow: true }
+            );
+            setPagination({ pageNum, pageSize });
+          }}
+        />
       </div>
     </BaseLayout>
   );
 };
 
-const Posts = ({ topic, posts, user, fetchMore, count }) => {
+const Posts = ({
+  topic,
+  posts,
+  user,
+  fetchMore,
+  onPageChange,
+  ...pagination
+}) => {
   const pageEnd = useRef();
   const [isReplierOpen, setReplierOpen] = useState(false);
   const [replyTo, setReplyTo] = useState(null);
   const [createPost, { error }] = useCreatePost();
+  const { pageSize, count, pageNum } = pagination;
 
   const handleCreatePost = async reply => {
     if (replyTo) {
@@ -60,14 +87,20 @@ const Posts = ({ topic, posts, user, fetchMore, count }) => {
     }
     reply.topic = topic._id;
     await createPost({ variables: reply });
-    fetchMore({
-      updateQuery: (prevResults, { fetchMoreResult }) => {
-        return {
-          ...prevResults,
-          postsByTopic: [...fetchMoreResult.postsByTopic],
-        };
-      },
-    });
+    let lastPage = Math.ceil(count / pageSize);
+    if (count === 0) {
+      lastPage = 1;
+    }
+    lastPage === pageNum &&
+      fetchMore({
+        variables: { pageSize, pageNum: lastPage },
+        updateQuery: (prevResults, { fetchMoreResult }) => {
+          return {
+            ...prevResults,
+            postsByTopic: { ...fetchMoreResult.postsByTopic },
+          };
+        },
+      });
     cleanAfterCreation();
     scrollToBottom();
   };
@@ -83,7 +116,9 @@ const Posts = ({ topic, posts, user, fetchMore, count }) => {
   return (
     <section style={{ marginBottom: '100px' }}>
       <div className="fj-post-list">
-        {topic._id && <PostItem post={topic} className="topic-post-lead" />}
+        {topic._id && pageNum === 1 && (
+          <PostItem post={topic} className="topic-post-lead" />
+        )}
         <div className="row" style={{ marginLeft: '100px' }}>
           <div className="col-md-9">
             {posts.map(post => (
@@ -117,7 +152,7 @@ const Posts = ({ topic, posts, user, fetchMore, count }) => {
               </div>
             )}
             <div className="pagination-container ml-auto">
-              <AppPagination count={count} />
+              <AppPagination {...pagination} onPageChange={onPageChange} />
             </div>
           </div>
         </div>
